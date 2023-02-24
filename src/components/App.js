268360da416218e0c9b9356dbaca980b261a613b
Kanha Choubey <kanhaarmyoo7@gmail.com>
2023-02-24 21:30:21 +0530

    file and folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+
+import { Movies, MovieInformation, NavBar, Profile, Actors } from "./";
+//   / -> root -> all movie
+//  /asd123 -> movie information -> more
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
